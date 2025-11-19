ef1f5729a25de873570309889a225f158ee341be
Nikhil Lanje <nikhillanje00@gmail.com>
2025-11-19 12:53:26 +0530

    Fix smpt server

--- SkillConnect/settings.py
+++ SkillConnect/settings.py
@@ -148,6 +148,19 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+else:
+    # Avoid crash on Render – just log emails
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 # ===============================
 # Gemini API Key
 # ===============================
